415cd2c6e666bffa32720616a32be2f48339fdb7
YooSangHyeon <yoosanghyeon1@gmail.com>
2022-03-31 17:24:34 +0900

    tested/test_my_video

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
+app.get("/", (_, res) => res.render("test"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const port = 2000;
