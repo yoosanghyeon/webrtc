bd6788c4742544378ba5d5f348b1c60f76db135b
YooSangHyeon <yoosanghyeon1@gmail.com>
2022-04-07 17:39:59 +0900

    fiexd/turn_server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -276,16 +276,11 @@ async function makeConnection(socketId) {
   const myPeerConnection = new RTCPeerConnection({
     iceServers: [
       {
-        urls: [
-          // "stun:stun.l.google.com:19302",
-          // "stun:stun1.l.google.com:19302",
-          // "stun:stun2.l.google.com:19302",
-          // "stun:stun3.l.google.com:19302",
-          // "stun:stun4.l.google.com:19302",
-          "turn:211.119.132.242:3478"
-        ],
-      },
-    ],
+        urls: 'turn:211.119.132.242:3478?transport=tcp',
+        credential: 'test123',
+        username: 'test'
+      }
+    ]
   });
 
 
