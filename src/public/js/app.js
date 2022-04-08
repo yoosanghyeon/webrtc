826aca1d69b4496ea4bd296b79e07df8df04e425
YooSangHyeon <yoosanghyeon1@gmail.com>
2022-04-08 16:31:18 +0900

    func/google-stun-add

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -281,6 +281,21 @@ async function makeConnection(socketId) {
   const myPeerConnection = new RTCPeerConnection({
     iceServers: [
       {
+        urls: 'stun:stun.l.google.com:19302',
+      },
+      {
+        urls: 'stun:stun1.l.google.com:19302',
+      },
+      {
+        urls: 'stun:stun2.l.google.com:19302',
+      },
+      {
+        urls: 'stun:stun3.l.google.com:19302',
+      },
+      {
+        urls: 'stun:stun4.l.google.com:19302',
+      },
+      {
         urls: 'turn:211.119.132.242:3478?transport=tcp',
         credential: 'test123',
         username: 'test'
